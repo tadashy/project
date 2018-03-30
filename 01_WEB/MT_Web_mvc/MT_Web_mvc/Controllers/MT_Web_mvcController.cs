ac047f647e2d36aed8b3b3ee653449373c9f359a
tadashy <kokomaru0320@yahoo.co.jp>
2018-03-31 07:06:24 +0900

    Asp.net のmvcの勉強　ログイン画面　ユーザメンテナンス画面を作成してみる。

---
+++ 01_WEB/MT_Web_mvc/MT_Web_mvc/Controllers/MT_Web_mvcController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MT_Web_mvc.Controllers
+{
+    public class MT_Web_mvcController : Controller
+    {
+        // GET: MT_Web_mvc
+        public ActionResult Index()
+        {
+            ViewData["msg"] = "こんにちは！ MVC頑張ろう！";
+            return View();
+        }
+    }
+}
---
+++ 01_WEB/MT_Web_mvc/MT_Web_mvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MT_Web_mvc.MvcApplication" Language="C#" %>
