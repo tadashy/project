f41a921a9b846a12003b33763336d4461df4b0bc
tadashy <kokomaru0320@yahoo.co.jp>
2018-05-20 06:49:15 +0900

    最新版をコミット

---
+++ 01_WEB/TEST/TEST/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TEST.Models
+{
+    public class User                                                   
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Passwprd { get; set; }
+        public int Gender { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Memo { get; set; }
+    }
+}
